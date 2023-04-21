03b9e96b298e20af20119dcf5833524eb1d55059
Giovanni Palusa <giovanni@palusa.se>
2023-04-21 18:00:21 +0200

    updated start screen

--- lib/RecipeGPTApp.dart
+++ lib/RecipeGPTApp.dart
@@ -17,77 +17,87 @@ class RecipeGPTApp extends StatelessWidget {
             height: double.infinity,
             width: double.infinity,
           ),
-          ClipRect(
-            child: BackdropFilter(
-              filter: ui.ImageFilter.blur(
-                sigmaX: 5.0,
-                sigmaY: 5.0,
-              ),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.black.withOpacity(0.2),
+          Positioned(
+            top: 60.0,
+            left: 0,
+            right: 0,
+            child: Center(
+              child: Text(
+                'Recipe Wizard',
+                style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black,
                 ),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Flexible(
-                        child: Image.asset(
-                          'assets/recipe-wizard.png',
-                          fit: BoxFit.contain,
-                        ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 16.0,
+            right: 16.0,
+            child: Container(
+              padding: EdgeInsets.only(bottom: 24.0),
+              child: Center(
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16.0),
+                  child: BackdropFilter(
+                    filter: ui.ImageFilter.blur(
+                      sigmaX: 5.0,
+                      sigmaY: 5.0,
+                    ),
+                    child: Container(
+                      padding: EdgeInsets.symmetric(horizontal: 32.0, vertical: 16.0),
+                      decoration: BoxDecoration(
+                        color: Colors.black.withOpacity(0.2),
                       ),
-                      SizedBox(height: 16.0),
-                      Text(
-                        'Recipe Wizard',
-                        style: TextStyle(
-                          fontSize: 32.0,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 16.0),
-                      Text(
-                        'Din guide till ett kulinariskt mästerverk. Typ.',
-                        style: TextStyle(
-                          fontSize: 24.0,
-                          color: Colors.white,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                      SizedBox(height: 32.0),
-                      ElevatedButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => InputScreen()),
-                          );
-                        },
-                        style: ElevatedButton.styleFrom(
-                          primary: Colors.white,
-                          padding: EdgeInsets.symmetric(
-                            horizontal: 48.0,
-                            vertical: 16.0,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: <Widget>[
+                          Text(
+                            'Din guide till ett kulinariskt mästerverk. Typ.',
+                            style: TextStyle(
+                              fontSize: 24.0,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
                           ),
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50),
+                          SizedBox(height: 32.0),
+                          ElevatedButton(
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => InputScreen()),
+                              );
+                            },
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 48.0,
+                                vertical: 16.0,
+                              ),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              elevation: 5.0,
+                              animationDuration: Duration(milliseconds: 200),
+                              shadowColor: Colors.black.withOpacity(0.5),
+                            ),
+                            child: Text(
+                              'Start',
+                              style: TextStyle(
+                                fontSize: 24.0,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blue[900],
+                              ),
+                            ),
                           ),
-                          elevation: 5.0,
-                          animationDuration: Duration(milliseconds: 200),
-                          shadowColor: Colors.black.withOpacity(0.5),
-                        ),
-                        child: Text(
-                          'Start',
-                          style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue[900],
-                          ),
-                        ),
+                          SizedBox(height: 16.0)
+                        ],
                       ),
-                      SizedBox(height: 40.0)
-                    ],
+                    ),
                   ),
                 ),
               ),
