24f7607e12c4c6e7b5fe23bad935531d58e58bfb
John <john.abdulnoor@sosalarm.se>
2023-04-21 14:23:08 +0200

    Added code to get recipes from a list of food items. API is imaginary

---
+++ lib/models/recipe.dart
@@ -0,0 +1,16 @@
+class Recipe {
+  final String title;
+  final String imageUrl;
+
+  Recipe({
+    required this.title,
+    required this.imageUrl,
+  });
+
+  factory Recipe.fromJson(Map<String, dynamic> json) {
+    return Recipe(
+      title: json['title'],
+      imageUrl: json['imageUrl'],
+    );
+  }
+}
