d98b9a0e253c3c3e980a035767df9ce0b14e529a
Martin Söderstrand <masoov131@online.rf.se>
2023-04-21 12:03:51 +0200

    Added input field.

--- lib/RecipeGPTApp.dart
+++ lib/RecipeGPTApp.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 import 'InputScreen.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+
+  final List<String> _suggestions = [
+    'Chocolate Chip Cookies',
+    'Lasagna',
+    'Spaghetti Carbonara',
+    'Grilled Cheese Sandwich',
+    'Roast Chicken',
+  ];
+  String? _selectedRecipe;
 
 class RecipeGPTApp extends StatelessWidget {
   const RecipeGPTApp({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -22,12 +33,39 @@ class RecipeGPTApp extends StatelessWidget {
               ),
             ),
             SizedBox(height: 16.0),
+            TypeAheadField(
+              textFieldConfiguration: TextFieldConfiguration(
+                decoration: InputDecoration(
+                  labelText: 'Search for a recipe',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              suggestionsCallback: (pattern) {
+                return _suggestions
+                    .where((recipe) =>
+                        recipe.toLowerCase().contains(pattern.toLowerCase()))
+                    .toList();
+              },
+              itemBuilder: (context, suggestion) {
+                return ListTile(
+                  title: Text(suggestion),
+                );
+              },
+              onSuggestionSelected: (suggestion) {
+                _selectedRecipe = suggestion;
+              },
+            ),
+            SizedBox(height: 16.0),
             ElevatedButton(
               onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => InputScreen()),
-                );
+                if (_selectedRecipe != null) {
+                   Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => InputScreen()),
+                      );
+                } else {
+                  // Show an error message.
+                }
               },
               child: Text('Start'),
             ),
