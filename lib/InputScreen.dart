39976bb6f8947c357fce70bbf1bddc71c3226f4c
Patrik Johansson <patrik.g.johansson@knowit.se>
2023-04-21 14:29:38 +0200

    added some general styling and input field with options to choose from

--- lib/InputScreen.dart
+++ lib/InputScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 class InputScreen extends StatefulWidget {
   const InputScreen({Key? key}) : super(key: key);
@@ -8,19 +9,101 @@ class InputScreen extends StatefulWidget {
 }
 
 class _InputScreenState extends State<InputScreen> {
+  List<String> selectedProducts = [];
+
+  final List<String> foodProducts = [    'Apple',    'Banana',    'Broccoli',    'Carrot',    'Celery',    'Cheese',    'Egg',    'Fish',    'Garlic',    'Lettuce',    'Milk',    'Orange',    'Potato',    'Spinach',    'Tomato',    'Turkey',    'Watermelon',    'Yogurt',    'Zucchini',  ];
+
+  final TextEditingController _typeAheadController = TextEditingController();
+
+  @override
+  void dispose() {
+    _typeAheadController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Input Screen'),
       ),
-      body: Center(
-        child: Text(
-          'This is the input screen',
-          style: TextStyle(
-            fontSize: 24.0,
-            fontWeight: FontWeight.bold,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFFD9AFD9),
+              Color(0xFF97D9E1),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
           ),
+        ),
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TypeAheadField(
+              textFieldConfiguration: TextFieldConfiguration(
+                controller: _typeAheadController,
+                decoration: InputDecoration(
+                  labelText: 'Search for food products',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              suggestionsCallback: (pattern) {
+                return foodProducts
+                    .where((product) =>
+                        product.toLowerCase().contains(pattern.toLowerCase()))
+                    .toList();
+              },
+              itemBuilder: (context, suggestion) {
+                return ListTile(
+                  title: Text(suggestion),
+                );
+              },
+              onSuggestionSelected: (suggestion) {
+                _typeAheadController.text = '';
+                setState(() {
+                  selectedProducts.add(suggestion);
+                });
+              },
+            ),
+            SizedBox(height: 16.0),
+            Wrap(
+              spacing: 8.0,
+              children: [
+                for (final product in selectedProducts)
+                  Chip(
+                    label: Text(product),
+                    deleteIcon: Icon(Icons.clear),
+                    onDeleted: () {
+                      setState(() {
+                        selectedProducts.remove(product);
+                      });
+                    },
+                  ),
+              ],
+            ),
+            SizedBox(height: 16.0),
+            ElevatedButton(
+              onPressed: () {
+                print({'selected_products': selectedProducts});
+              },
+              style: ElevatedButton.styleFrom(
+                primary: Colors.white,
+                onPrimary: Colors.black,
+                padding: EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+                textStyle: TextStyle(
+                  fontSize: 18.0,
+                  fontWeight: FontWeight.bold,
+                ),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                elevation: 10,
+              ),
+              child: Text('Submit'),
+            ),
+          ],
         ),
       ),
     );
--- lib/RecipeGPTApp.dart
+++ lib/RecipeGPTApp.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
 import 'InputScreen.dart';
-import 'package:flutter_typeahead/flutter_typeahead.dart';
-
-  final List<String> _suggestions = [
-    'Chocolate Chip Cookies',
-    'Lasagna',
-    'Spaghetti Carbonara',
-    'Grilled Cheese Sandwich',
-    'Roast Chicken',
-  ];
-  String? _selectedRecipe;
 
 class RecipeGPTApp extends StatelessWidget {
   const RecipeGPTApp({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -21,55 +10,62 @@ class RecipeGPTApp extends StatelessWidget {
       appBar: AppBar(
         title: Text('Recipe GPT'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Welcome to Recipe GPT!',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 16.0),
-            TypeAheadField(
-              textFieldConfiguration: TextFieldConfiguration(
-                decoration: InputDecoration(
-                  labelText: 'Search for a recipe',
-                  border: OutlineInputBorder(),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Colors.blue[300]!, Colors.blue[900]!],
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Text(
+                  'Welcome to our Recipe AI Generator!',
+                  style: TextStyle(
+                    fontSize: 32.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
                 ),
               ),
-              suggestionsCallback: (pattern) {
-                return _suggestions
-                    .where((recipe) =>
-                        recipe.toLowerCase().contains(pattern.toLowerCase()))
-                    .toList();
-              },
-              itemBuilder: (context, suggestion) {
-                return ListTile(
-                  title: Text(suggestion),
-                );
-              },
-              onSuggestionSelected: (suggestion) {
-                _selectedRecipe = suggestion;
-              },
-            ),
-            SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                if (_selectedRecipe != null) {
-                   Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => InputScreen()),
-                      );
-                } else {
-                  // Show an error message.
-                }
-              },
-              child: Text('Start'),
-            ),
-          ],
+              SizedBox(height: 32.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => InputScreen()),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.white,
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 48.0,
+                    vertical: 16.0,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                  elevation: 5.0,
+                  animationDuration: Duration(milliseconds: 200),
+                  shadowColor: Colors.black.withOpacity(0.5),
+                ),
+                child: Text(
+                  'Start',
+                  style: TextStyle(
+                    fontSize: 24.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blue[900],
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
