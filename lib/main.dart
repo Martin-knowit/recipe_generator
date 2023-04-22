8a6348facbb9815044db4345bd559eeb17ef4c0a
Giovanni Palusa <giovanni@palusa.se>
2023-04-22 09:19:25 +0200

    added icon and mac os network allowed

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Recipe GPT App',
+      title: 'Recipe Wizard',
       home: const RecipeGPTApp(),
     );
   }
